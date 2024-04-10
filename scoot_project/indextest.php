35c630358f73f246f54b625f2d6032064f11c79b
KennyWouters <kenny.wouters2@gmail.com>
2024-04-10 15:51:33 +0200

    automatic head and foot test

---
+++ scoot_project/indextest.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="./dist/assets/index-C_WgrVZA.css">
+<?php include './views/header.php'; ?>
+<?php include './views/footer.php';
---
+++ scoot_project/views/footer.php
@@ -0,0 +1,49 @@
+<footer class="">
+    <div class="footer_up">
+        <div class="container footer_container">
+            <div>
+                <p>Sign up and Scoot off today</p>
+            </div>
+            <div class="stores">
+                <button>
+                    <img src="assets/svg/footer/appStore.svg" alt="apple app store link logo">
+                </button>
+                <button>
+                    <img src="assets/svg/footer/googlePlay.svg" alt="android google play store link logo">
+                </button>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="footer_down ">
+
+        <div class="container footer_container">
+            <div class="l">
+                <img src="./assets/svg/header/scoot.svg" alt="">
+                <nav class="nav">
+                    <ul>
+                        <li>
+                            <a href="about.html">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Location</a>
+                        </li>
+                        <li>
+                            <a href="#">Careers</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div class="right">
+                <div class="social">
+                    <img src="./assets/svg/footer/facebook.svg" alt="facebook logo">
+                    <img src="./assets/svg/footer/instagram.svg" alt="instagram logo">
+                    <img src="./assets/svg/footer/twitter.svg" alt="twitter logo">
+                </div>
+            </div>
+        </div>
+
+</footer>
